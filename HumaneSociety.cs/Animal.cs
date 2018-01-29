db50b8b5b5e72fc09612a15f290ff0e4a9087a46
Curtis Hansen <curtishansen14@gmail.com>
2018-01-29 08:26:14 -0600

    built animal class first draft

---
+++ HumaneSociety.cs/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety.cs
+{
+    class Animal
+    {
+        private bool status;
+
+        public Animal()
+        {
+            status = false;
+        }
+
+        public bool Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+    }
+}

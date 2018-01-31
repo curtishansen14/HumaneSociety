98a17c2f12a15250827b52b10072a040f6a12b3c
Curtis Hansen <curtishansen14@gmail.com>
2018-01-31 15:55:26 -0600

    stupped out dataconverter class

---
+++ HumaneSociety.cs/DataTypeConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety.cs
+{
+    class DataTypeConverter
+    {
+        //String to Datetime
+        //string to int
+        //string to decimal 
+
+    }
+}
